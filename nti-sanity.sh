323ff6f1d110f6401300fb7954fb3e1d28f0da4f
nic-instruction <nicole.bade@seattlecolleges.edu>
2017-05-20 15:07:41 -0700

    Update nti-sanity.sh

--- nti-sanity.sh
+++ nti-sanity.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # This is a script meant to let us test NRPE
 # Plugins that we write
-status="0"
+
+status="0"                                  # Change the status to test different alert states
+
 if [ $status == "0" ]; then
     echo "STATUS:OK"
     exit 0;
+    
   elif [ $status == "2" ]; then
     echo "STATUS:CRITICAL"
     exit 2;
+    
   elif [ $status == "1" ] ; then
     echo "STATUS:WARNING"
     exit 1;
